b591b77d0937b1782063fc1c6972e03e549e8d8f
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-19 16:37:08 +0000

    add package info file for contact module

---
+++ src/main/java/com/app/cms/contact/package-info.java
@@ -0,0 +1,8 @@
+@ApplicationModule(
+        displayName = "Contact Management",
+        allowedDependencies = {}
+)
+
+package com.app.cms.contact;
+import org.springframework.modulith.ApplicationModule;
+
