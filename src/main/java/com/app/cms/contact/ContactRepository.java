3a71d60ae806436dd76a7f75a61b22659c4a4a97
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-20 10:08:27 +0000

    feat: find by tags method with query

--- src/main/java/com/app/cms/contact/ContactRepository.java
+++ src/main/java/com/app/cms/contact/ContactRepository.java
@@ -3,6 +3,7 @@ package com.app.cms.contact;
 import com.app.cms.contact.domain.ContactEntity;
 import com.app.cms.contact.domain.ContactStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,7 @@ import java.util.Optional;
 public interface ContactRepository extends JpaRepository<ContactEntity,Long> {
     Optional<ContactEntity> findByEmail(String email);
     List<ContactEntity> findByStatus(ContactStatus status);
+
+    @Query("SELECT c FROM ContactEntity c JOIN c.tags t WHERE t.id IN :tagIds")
+    List<ContactEntity> findByTagIds(List<Long> tagIds);
 }
