19ba4afa9de43897d7a92eeae6e7548b33453428
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-20 10:18:16 +0000

    feat: create contact dto

---
+++ src/main/java/com/app/cms/contact/ContactDto.java
@@ -0,0 +1,29 @@
+package com.app.cms.contact;
+
+import com.app.cms.contact.domain.ContactStatus;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.Set;
+
+@Data
+public class ContactDto {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    private String phone;
+
+    private String company;
+
+    private ContactStatus status;
+
+    private Set<Long> tagIds;
+}
