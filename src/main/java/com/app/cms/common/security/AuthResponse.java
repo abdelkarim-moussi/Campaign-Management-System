b2ee4f6cda4fa51c08b6bd95a1ad43ec197106b5
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-28 16:39:28 +0000

    implement AuthResponse dto

--- src/main/java/com/app/cms/common/security/AuthResponse.java
+++ src/main/java/com/app/cms/common/security/AuthResponse.java
@@ -1,4 +1,16 @@
 package com.app.cms.common.security;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
 public class AuthResponse {
+    String accessToken;
+    String refreshToken;
 }
