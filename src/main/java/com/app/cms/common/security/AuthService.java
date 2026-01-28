ef08c2a42ae36eb8782e92d7588e7bd01c034ffa
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-28 16:40:26 +0000

    feat: implement authService, authenticate method

--- src/main/java/com/app/cms/common/security/AuthService.java
+++ src/main/java/com/app/cms/common/security/AuthService.java
@@ -1,4 +1,40 @@
 package com.app.cms.common.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public ResponseEntity<AuthResponse> authenticate(AuthRequest authRequest){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getEmail(),authRequest.getPassword())
+        );
+
+        if(authentication.isAuthenticated()){
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+            Map<String,String> tokens = jwtService.generateTokenPair(userDetails);
+            return ResponseEntity.ok(AuthResponse.builder()
+                    .accessToken(tokens.get("accessToken"))
+                    .refreshToken(tokens.get("refreshToken"))
+                    .build());
+        }else {
+            throw new UsernameNotFoundException("Invalid user request");
+        }
+    }
+
 }
