e483c5dffc8e344bd75bc34f3aa1877234697f23
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-28 12:15:14 +0000

    feat: implement generate token method

--- src/main/java/com/app/cms/common/security/JwtService.java
+++ src/main/java/com/app/cms/common/security/JwtService.java
@@ -2,9 +2,15 @@ package com.app.cms.common.security;
 
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtService {
@@ -22,6 +28,22 @@ public class JwtService {
         final Claims claims =extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
+    public String generateToken(UserDetails userDetails){
+        Map<String,Object> claims = new HashMap<>();
+        Set<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .filter(authority -> authority.startsWith("ROLE_"))
+                .collect(Collectors.toSet());
+        claims.put("roles",roles);
 
+        Set<String> permissions = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .filter(authority -> !authority.startsWith("ROLE_"))
+                .collect(Collectors.toSet());
+
+        claims.put("permissions",permissions);
+
+        return buildToken(claims,userDetails.getUsername(),expirationTime);
+    }
 
 }
