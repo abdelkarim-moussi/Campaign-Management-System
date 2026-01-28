04e4d4bd27e4ca7c5fa3ca1c73166ef88bfc5e04
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-28 10:49:20 +0000

    feat: start implementing jwtService

---
+++ src/main/java/com/app/cms/common/security/JwtService.java
@@ -0,0 +1,25 @@
+package com.app.cms.common.security;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class JwtService {
+    @Value("security.jwt.secret.key")
+    private String secretKey;
+
+    @Value("security.jwt.expiration-time")
+    private long expirationTime;
+
+    public String extractUserName(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims =extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+}
