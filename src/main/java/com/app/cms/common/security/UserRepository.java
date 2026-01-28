491d2f102ebb89f3e8d30f6802c2b628a8f55458
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-28 16:40:45 +0000

    feat: implement user repository

--- src/main/java/com/app/cms/common/security/UserRepository.java
+++ src/main/java/com/app/cms/common/security/UserRepository.java
@@ -1,4 +1,6 @@
 package com.app.cms.common.security;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity,String> {
 }
