425e06763eacc8c0e2f8c4f90fc4d897ea8feb4c
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-28 09:55:15 +0000

    feat: recreate the project

---
+++ src/main/java/com/app/cms/CampaignManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.app.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampaignManagementSystemApplication.class, args);
+    }
+
+}
