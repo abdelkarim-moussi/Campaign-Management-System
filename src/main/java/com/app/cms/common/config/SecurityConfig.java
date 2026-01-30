e4ba89e225c8c4806937a93acb955811c4edf23a
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-30 11:27:43 +0000

    feat: implement authentication provider and authentication manager configuration

--- src/main/java/com/app/cms/common/config/SecurityConfig.java
+++ src/main/java/com/app/cms/common/config/SecurityConfig.java
@@ -2,7 +2,12 @@ package com.app.cms.common.config;
 
 import com.app.cms.common.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,6 +29,7 @@ public class SecurityConfig {
     private final PasswordEncoder passwordEncoder;
     private final CorsConfigurationSource corsConfigurationSource;
 
+    @Bean
     public SecurityFilterChain securityFilterChai(HttpSecurity http) throws Exception{
     http.csrf(AbstractHttpConfigurer::disable)
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
@@ -42,4 +48,20 @@ public class SecurityConfig {
 
     return http.build();
 }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+
 }
