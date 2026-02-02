7186e02957a1b4b90e9fc26e23b8d766dee76d04
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-02-02 09:29:37 +0000

    feat: implement package-info.java file for the temeplate module

---
+++ src/main/java/com/app/cms/template/package-info.java
@@ -0,0 +1,8 @@
+@ApplicationModule(
+        displayName = "Template Management",
+        allowedDependencies = {}
+)
+
+package com.app.cms.template;
+
+import org.springframework.modulith.ApplicationModule;
