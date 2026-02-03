92a4cdc00805c6f74ce1ea2d37a073088ea503e3
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-02-03 14:59:04 +0000

    feat: clean up the main class

--- src/main/java/com/app/cms/CampaignManagementSystemApplication.java
+++ src/main/java/com/app/cms/CampaignManagementSystemApplication.java
@@ -1,12 +1,9 @@
 package com.app.cms;
 
-import com.app.cms.common.security.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class CampaignManagementSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(CampaignManagementSystemApplication.class, args);
