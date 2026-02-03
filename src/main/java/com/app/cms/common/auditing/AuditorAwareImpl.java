87fa2f2250a540ce788b7e66487c305dff092375
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-02-03 15:21:59 +0000

    feat: create an auditor aware implementation to get the authenticated user and set the createdBy field to it

---
+++ src/main/java/com/app/cms/common/auditing/AuditorAwareImpl.java
@@ -0,0 +1,24 @@
+package com.app.cms.common.auditing;
+
+import com.app.cms.common.exception.UserNotFoundException;
+import com.app.cms.common.security.User;
+import com.app.cms.common.security.UserRepository;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditorProvider")
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null){
+            return Optional.of(auth.getName());
+        }
+        return Optional.of("UNKNOWN");
+    }
+}
