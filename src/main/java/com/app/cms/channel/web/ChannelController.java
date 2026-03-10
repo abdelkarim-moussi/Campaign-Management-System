eb6f4c52360de6967eea36887ec8b7ff6d181c00
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-03-10 10:16:32 +0000

    refactor

--- src/main/java/com/app/cms/channel/web/ChannelController.java
+++ src/main/java/com/app/cms/channel/web/ChannelController.java
@@ -2,14 +2,12 @@ package com.app.cms.channel.web;
 
 import com.app.cms.channel.ChannelService;
 import com.app.cms.channel.MessageSent;
-import com.app.cms.channel.MessageStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/channels")
