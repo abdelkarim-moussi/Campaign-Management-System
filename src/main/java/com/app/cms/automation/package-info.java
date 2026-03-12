42045993d2428112b6fc7afbbeb19520e53ea719
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-03-12 22:17:16 +0000

    feat: define the automation module and it depnedencies

--- src/main/java/com/app/cms/automation/package-info.java
+++ src/main/java/com/app/cms/automation/package-info.java
@@ -1,1 +1,7 @@
+@ApplicationModule(
+        displayName = "Marketing Automation",
+        allowedDependencies = {"contact","template","channel","campaign"}
+)
 package com.app.cms.automation;
+
+import org.springframework.modulith.ApplicationModule;
