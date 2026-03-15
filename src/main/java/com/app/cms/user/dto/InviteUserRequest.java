577545968d28baa2c4bbaa3687c6974114c75711
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-03-15 00:37:25 +0000

    feat: create test cases for user service

--- src/main/java/com/app/cms/user/dto/InviteUserRequest.java
+++ src/main/java/com/app/cms/user/dto/InviteUserRequest.java
@@ -4,9 +4,11 @@ import com.app.cms.user.entity.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class InviteUserRequest {
 
     @Email(message = "Invalid email format")
