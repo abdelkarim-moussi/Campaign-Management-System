bebd2b81918d2594cc78f11a12cb713d0a69161b
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-03-17 11:57:16 +0000

    fix: environement file loading failure

--- src/main/java/com/app/cms/CampaignManagementSystemApplication.java
+++ src/main/java/com/app/cms/CampaignManagementSystemApplication.java
@@ -1,11 +1,22 @@
 package com.app.cms;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CampaignManagementSystemApplication {
+
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
         SpringApplication.run(CampaignManagementSystemApplication.class, args);
     }
 
